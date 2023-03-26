11ab6007b700226b418ab9444993742cdc05e9e5
DylanDev92 <changliangdylan@gmail.com>
2023-03-25 22:32:17 -0400

    upload content

---
+++ src/pages/api/projectlist.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+
+var list = JSON.parse(fs.readFileSync("./data/myprojects.json"));
+
+export default function handler(req, res) {
+
+  console.log(list);
+
+  res.status(200).json(list);
+}
